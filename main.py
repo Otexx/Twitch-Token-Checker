9f185d0702f99bf208889ad05ff6c6bef62e0b82
Otex <121303662+Otexx@users.noreply.github.com>
2023-04-22 12:25:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 def check_token(token):
     headers = {
@@ -7,14 +8,20 @@ def check_token(token):
     response = requests.get('https://id.twitch.tv/oauth2/validate', headers=headers)
     if response.status_code == 200:
         print(f"\033[32mToken is valid!\033[0m")
-        with open('valid_tokens.txt', 'a') as f:
+        with open('Results/valid_tokens.txt', 'a') as f:
             f.write(token + '\n')
         return True
     else:
         print(f"\033[31mToken is invalid.\033[0m")
-        with open('invalid_tokens.txt', 'a') as f:
+        if not os.path.exists('Results'):
+            os.makedirs('Results')
+        with open('Results/invalid_tokens.txt', 'a') as f:
             f.write(token + '\n')
         return False
+
+open('Results/valid_tokens.txt', 'w').close()
+
+open('Results/invalid_tokens.txt', 'w').close()
 
 with open('tokens.txt', 'r') as f:
     tokens = f.read().splitlines()
@@ -22,4 +29,4 @@ with open('tokens.txt', 'r') as f:
 for token in tokens:
     check_token(token)
 
-print(f"\033[34mFinished Checking\n Valid Tokens: {len(open('valid_tokens.txt').readlines())} | Invalid Tokens: {len(open('invalid_tokens.txt').readlines())}\033[0m\n")
+print(f"\033[34mFinished Checking\n Valid Tokens: {len(open('Results/valid_tokens.txt').readlines())} | Invalid Tokens: {len(open('Results/invalid_tokens.txt').readlines())}\033[0m\n")
